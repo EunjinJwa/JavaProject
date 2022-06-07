9e86bc13fb509ddd0b0c0273472ef18c609688b1
EunjinJwa <ejjwa0563@gmail.com>
2022-06-07 22:26:28 +0900

    maven project 변환

--- src/test/java/jinny/study/refactoring/StatementTest.java
+++ src/test/java/jinny/study/refactoring/StatementTest.java
@@ -2,10 +2,20 @@ package jinny.study.refactoring;
 
 import jinny.study.refactoring.domain.Invoice;
 import jinny.study.refactoring.domain.InvoicePerformance;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-public class StatementTest {
+class StatementTest {
 
-    public static void main(String[] args) {
+    @Test
+    void statement() {
+        String resultString = "청구 내역 (고객명: BigCo)\n" +
+                "Hamlet: $650.00 (55석)\n" +
+                "As You Like It: $580.00 (35석)\n" +
+                "Othello: $500.00 (40석)\n" +
+                "총액: $1730.00\n" +
+                "적립 포인트: 47점\n";
+
         Invoice invoice = new Invoice();
         invoice.customer = "BigCo";
         invoice.addPerformance(new InvoicePerformance("hamlet", 55));
@@ -14,6 +24,9 @@ public class StatementTest {
 
         Statement statement = new Statement();
         String result = statement.statement(invoice, null);
+
+        Assertions.assertThat(resultString).isEqualTo(result);
         System.out.println(result);
+
     }
 }
