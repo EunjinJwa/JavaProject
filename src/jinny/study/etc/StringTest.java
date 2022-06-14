5182fd38672454e3e98fff3b1d78739e8c2aeda6
EunjinJwa <ejjwa0563@gmail.com>
2022-06-14 17:27:05 +0900

    String test

--- src/jinny/study/etc/StringTest.java
+++ src/jinny/study/etc/StringTest.java
@@ -10,8 +10,12 @@ public class StringTest {
      * 정규 표현식 문자열 치환
      */
     public static void replaceRegex() {
-        String str = "TEST_123/and:colone!";
-        String regex = "[:!/]";
+        String str = "TEST_\\123/and:{colo<>ne}!".concat("\\");
+        System.out.println(str);
+        String regex = "[$¥*{:<}>|/?\\W\"!]";  // 역슬러시 변환은 \\W 뒤에 작성해야함.
         System.out.println(str.replaceAll(regex, " "));
+        System.out.println(str.replaceAll(regex, " ")
+                .replace("\\", ""));
+
     }
 }
