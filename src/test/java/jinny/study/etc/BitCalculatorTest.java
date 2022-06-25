d234f7da5ebc86f890740c888a558c77d3554372
EunjinJwa <ejjwa0563@gmail.com>
2022-06-25 18:15:02 +0900

    Bit calculator Test

---
+++ src/test/java/jinny/study/etc/BitCalculatorTest.java
@@ -0,0 +1,15 @@
+package jinny.study.etc;
+
+import org.junit.jupiter.api.Test;
+
+public class BitCalculatorTest {
+
+    @Test
+    public void bitShiftTest() {
+        System.out.println("AllIn : " + (1 << 2));
+        System.out.println("RockPapperScissor : " + (1 << 3));
+
+    }
+
+
+}
