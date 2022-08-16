3d6ce9f738a81d33edd629cbaf24b3c0d4d89a6a
EunjinJwa <ejjwa0563@gmail.com>
2022-08-16 21:38:26 +0900

    template method pattern

---
+++ src/main/java/jinny/study/pattern/templatemethod/Application.java
@@ -0,0 +1,25 @@
+package jinny.study.pattern.templatemethod;
+
+public abstract class Application {
+    private boolean isDone = false;
+
+    protected abstract void init ();
+    protected abstract void idle ();
+    protected abstract void cleanup ();
+
+    protected void setDone() {
+        isDone = true;
+    }
+
+    protected boolean done() {
+        return isDone;
+    }
+
+    public void run() {
+        init();
+        while (!done()) {
+            idle();
+        }
+        cleanup();
+    }
+}
