b65311d4c31a55192b30198e217fd3c21d8ac3b6
EunjinJwa <ejjwa0563@gmail.com>
2024-12-23 22:16:04 +0900

    test 코드 정리

--- src/test/java/jinny/study/tdd/UserRegisterMockTest.java
+++ src/test/java/jinny/study/tdd/UserRegisterMockTest.java
@@ -21,16 +21,6 @@ public class UserRegisterMockTest {
         userRegister = new UserRegister(mockWeakPasswordChecker, memoryUserRepository, mockEmailNotifier);
     }
 
-    @DisplayName("약한 암호면 가입 실패")
-    @Test
-    void weakPassword () {
-        /** mockWeakPasswordChecker.checkPasswordWeak 가 실행되면 true를 반환하도록 설정함. */
-        BDDMockito.given(mockWeakPasswordChecker.checkPasswordWeak("pw1234"))
-                .willReturn(true);
-
-        Assertions.assertThatThrownBy(() -> userRegister.register(User.builder().id("100").name("jinny").email("jinny@email.com").build()))
-                .isInstanceOf(WeakPasswordException.class);
-    }
 
     @DisplayName("회원가입시 암호검사 수행함")
     @Test
